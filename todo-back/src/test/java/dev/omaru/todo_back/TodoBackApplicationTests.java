8da386576531033df28238614d71a29951ee18a1
miyashitaSora <sora.miyashita@rensa.co.jp>
2025-08-31 10:47:54 +0900

    first commit

---
+++ todo-back/src/test/java/dev/omaru/todo_back/TodoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.omaru.todo_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
